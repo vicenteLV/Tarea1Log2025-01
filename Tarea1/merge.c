10a1819e7f360ec8bea8d02274fe381b8ad13104
vicenteLV <vicentelopez@ug.uchile.cl>
2025-05-01 21:38:02 -0400

    avances

--- Tarea1/merge.c
+++ Tarea1/merge.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
+void merge(void* arreglo){
+
+    uint64_t p = len(arreglo)/2;
+
+    uint64_t mayores[p];
+    uint64_t menores[len(arreglo - p)];
+
+    for (int i=0; i<len(arreglo); i++){
+        
+
+    }
+
+}
